96f49ba33e84d582b87c4576a7dca05bfa1275e7
serj2626 <serj2626@mail.ru>
2025-02-03 19:40:56 +0300

    feat: upgrade structure

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,33 @@
-<script lang="ts" setup>
- 
-</script>
+<script lang="ts" setup></script>
 
 <template>
- <div class="app">
-  <aside class="sidebar"></aside>
-  <main class="main"></main>
-  <footer class="footer"></footer>
- </div>
+  <div class="app container">
+    <aside class="sidebar">sidebar</aside>
+    <main class="main">main</main>
+    <footer class="footer">footer</footer>
+  </div>
 </template>
 
 <style lang="scss" scoped>
- 
+.app {
+  display: grid;
+  grid-template-columns: 236px 1fr;
+  grid-template-rows: 1fr 72px;
+  gap: 24px;
+  grid-template-areas:
+    "sidebar main"
+    "footer footer";
+}
+.sidebar {
+  grid-area: sidebar;
+  background-color: lightgreen;
+}
+.main {
+  grid-area: main;
+  background-color: lightyellow;
+}
+.footer {
+  grid-area: footer;
+  background-color: lightskyblue;
+}
 </style>
