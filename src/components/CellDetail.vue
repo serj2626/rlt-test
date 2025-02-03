4b0d1b150be8ba24ede07fd1665ea34e4c1ecb25
serj2626 <serj2626@mail.ru>
2025-02-04 00:34:16 +0300

    feat: new component

---
+++ src/components/CellDetail.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+interface IProps {
+  color?: "green" | "blue" | "peru";
+}
+</script>
+
+<template>
+  <li class="cell-list__item"></li>
+</template>
+
+<style lang="scss" scoped></style>
