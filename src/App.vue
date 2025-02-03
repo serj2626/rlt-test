0d2276418299e1197cf0a60017ce50aaafb0011f
serj2626 <serj2626@mail.ru>
2025-02-03 19:27:36 +0300

    feat: upgrade structure file

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,11 @@
 </script>
 
 <template>
- 
+ <div class="app">
+  <aside class="sidebar"></aside>
+  <main class="main"></main>
+  <footer class="footer"></footer>
+ </div>
 </template>
 
 <style lang="scss" scoped>
