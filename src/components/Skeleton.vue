95fcdf1137061060ea9a69e08ca6d5410a2ce89c
serj2626 <serj2626@mail.ru>
2025-02-03 23:42:00 +0300

    feat: add dynamic class

--- src/components/Skeleton.vue
+++ src/components/Skeleton.vue
@@ -3,17 +3,18 @@ interface IPops {
   type: "sm" | "md" | "xl";
   paddingX?: number;
 }
+
+defineProps<IPops>();
 </script>
 
 <template>
-  <div class="skeleton"></div>
+  <div class="skeleton" :class="{ [`skeleton_${type}`]: type }"></div>
 </template>
 
 <style lang="scss" scoped>
 .skeleton {
   $paddingX: 14px;
   margin-inline: 23px - $paddingX;
-  height: 26px;
   background: linear-gradient(45deg, #3c3c3c 0%, #444444 51%, #333333 100%);
   border-radius: var(--border-radius-btn);
 
