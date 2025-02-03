07f275a96edb2799de28a569198d042248e361d3
serj2626 <serj2626@mail.ru>
2025-02-03 23:28:10 +0300

    feat: new ui component

---
+++ src/components/Skeleton.vue
@@ -0,0 +1,36 @@
+<script lang="ts" setup>
+interface IPops {
+  type: "sm" | "md" | "xl";
+  paddingX?: number;
+}
+</script>
+
+<template>
+  <div class="skeleton"></div>
+</template>
+
+<style lang="scss" scoped>
+.skeleton {
+  $paddingX: 14px;
+  margin-inline: 23px - $paddingX;
+  height: 26px;
+  background: linear-gradient(45deg, #3c3c3c 0%, #444444 51%, #333333 100%);
+  border-radius: var(--border-radius-btn);
+
+  &_margin {
+    margin-bottom: 24px;
+  }
+
+  &_sm {
+    height: 10px;
+  }
+
+  &_md {
+    height: 26px;
+  }
+
+  &_xl {
+    height: 36px;
+  }
+}
+</style>
