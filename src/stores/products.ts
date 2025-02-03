7fdaf6388df1862c80f6cf94e511f3df13496728
serj2626 <serj2626@mail.ru>
2025-02-04 00:33:52 +0300

    feat: new data by products

---
+++ src/stores/products.ts
@@ -0,0 +1,45 @@
+import { computed, ref } from "vue";
+
+import { defineStore } from "pinia";
+
+interface IProduct {
+  id: number;
+  name: string;
+  count: number;
+  color: string;
+  status: string;
+}
+
+export const useProductStore = defineStore("product", () => {
+  // STATE
+
+  const products = ref<IProduct[]>([
+    {
+      id: 1,
+      name: "Product 1",
+      count: 4,
+      color: "green",
+      status: "",
+    },
+    {
+      id: 2,
+      name: "Product 2",
+      count: 2,
+      color: "peru",
+      status: "",
+    },
+    {
+      id: 3,
+      name: "Product 3",
+      count: 5,
+      color: "blue",
+      status: "",
+    },
+  ]);
+
+  // GETTER
+
+  // SETTER
+
+  return { products };
+});
