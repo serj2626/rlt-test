3b9d9ec2a7a23bb78c1bd08006d3f25aa6c0c39a
serj2626 <serj2626@mail.ru>
2025-02-06 16:10:11 +0300

    feat: add code in footer component

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,11 +1,33 @@
-<script lang="ts" setup>
- 
-</script>
+<script lang="ts" setup></script>
 
 <template>
- 
+  <div class="footer__body">
+    <USkeleton class="footer__skeleton" type="xl" />
+  </div>
+  <button class="footer__btn">
+    <img
+      width="12"
+      height="12"
+      class="footer__btn-icon"
+      src="./assets/icons/close.svg"
+      alt="footer__btn-icon"
+      loading="lazy"
+    />
+  </button>
 </template>
 
 <style lang="scss" scoped>
- 
+.footer__body {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding-inline: 14px;
+}
+
+.footer__btn {
+  position: absolute;
+  right: 14px;
+  top: 14px;
+}
 </style>
