f74b1ea7ac6faf01251f87424a81fab55e4a4732
serj2626 <serj2626@mail.ru>
2025-02-06 14:39:00 +0300

    feat: add props and interface

--- src/components/UI/Button.vue
+++ src/components/UI/Button.vue
@@ -1,11 +1,26 @@
 <script lang="ts" setup>
- 
+interface IButton {
+  color: "red" | "white";
+}
+defineProps<IButton>();
 </script>
 
 <template>
- 
+  <button class="btn" :class="`btn_${color}`"><slot></slot></button>
 </template>
 
 <style lang="scss" scoped>
- 
+.btn {
+  &_white {
+    background-color: var(--color-white);
+    color: var(--color-text-dark);
+    font-size: 14px;
+  }
+
+  &_red {
+    background-color: var(--color-red);
+    color: var(--color-white);
+    font-size: 14px;
+  }
+}
 </style>
