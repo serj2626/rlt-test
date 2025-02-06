1578275cfc0ce481f858566e0e86003985c2f6fe
serj2626 <serj2626@mail.ru>
2025-02-07 02:46:25 +0300

    feat: add functions for drag and drop

--- src/components/CellList.vue
+++ src/components/CellList.vue
@@ -1,53 +1,67 @@
-<script lang="ts" setup>
+<script setup lang="ts">
+import { ref, onMounted } from "vue";
 import { useProductStore } from "../stores/products";
 import { storeToRefs } from "pinia";
-import CellDetail from "./CellDetail.vue";
 
 const { products } = storeToRefs(useProductStore());
+const cells = ref([...Array(25).keys()].map((x) => x + 1));
+
+function onDragStart(event: DragEvent, item: any) {
+  console.log(event, item);
+  // if (event.dataTransfer) {
+  //   event.dataTransfer.dropEffect = "move";
+  //   event.dataTransfer.effectAllowed = "move";
+  //   event.dataTransfer.setData("itemId", JSON.stringify(item.id));
+  // }
+}
+
+function onDrop(event: DragEvent, cellId: number) {
+  console.log(event, cellId);
+  // const itemId = parseInt(JSON.parse(event.dataTransfer.getData("itemId")));
+  // items.value = items.value.map((i) => {
+  //   if (i.id === itemId) {
+  //     return { ...i, categoryId };
+  //   }
+  //   return i;
+  // })
+}
 </script>
 
 <template>
-  <ul class="cell-list">
-    <CellDetail
-      v-for="product in products"
-      :key="product.id"
-      :product="product"
-    />
-
-    <div class="cell"></div>
-    <div class="cell"></div>
-    <div class="cell"></div>
-    <div class="cell"></div>
-    <div class="cell"></div>
-    <div class="cell"></div>
-    <div class="cell"></div>
-    <div class="cell"></div>
-    <div class="cell"></div>
-    <div class="cell"></div>
-    <div class="cell"></div>
-    <div class="cell"></div>
-    <div class="cell"></div>
-    <div class="cell"></div>
-    <div class="cell"></div>
-    <div class="cell"></div>
-    <div class="cell"></div>
-    <div class="cell"></div>
-    <div class="cell"></div>
-    <div class="cell"></div>
-    <div class="cell"></div>
-    <div class="cell"></div>
-  </ul>
+  <div class="cell-list">
+    <div
+      v-for="(cell, index) in cells"
+      :key="index"
+      class="cell-list__item"
+      droppable="true"
+      @dragover.prevent
+      @dragenter.prevent
+      @drop="onDrop($event, cell)"
+    >
+      {{ cell }}
+      <div v-for="product in products" :key="product.id">
+        {{ product }}
+      </div>
+    </div>
+  </div>
 </template>
 
-<style lang="scss" >
+<style>
 .cell-list {
   display: grid;
   grid-template-columns: repeat(5, 1fr);
   border-radius: var(--border-radius-block);
   overflow: hidden;
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
 }
 
+.cell-list__item {
+  width: 100%;
+  height: 100px;
+  border: var(--border);
+  transition: var(--transition-base);
+
+  &:hover {
+    background-color: var(--color-border);
+  }
+}
 </style>
