29cfdb734a49d0066e99bd407cff71275e9313bd
serj2626 <serj2626@mail.ru>
2025-02-06 23:17:31 +0300

    feat: add import button component

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,17 +1,10 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div class="footer__body">
-    <USkeleton class="footer__skeleton" type="xl" />
-  </div>
-  <!-- <button class="footer__btn">
-    <img
-      width="12"
-      height="12"
-      class="footer__btn-icon"
-      src="./assets/icons/close.svg"
-      alt="footer__btn-icon"
-      loading="lazy"
-    />
-  </button> -->
+  <footer class="footer">
+    <div class="footer__body">
+      <USkeleton class="footer__skeleton" type="xl" />
+    </div>
+    <UButton view="close" class="footer__btn-close btn-close_position" />
+  </footer>
 </template>
