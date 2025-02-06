cceb455c0e11e98b089b9ffc33cb86e0f6c6ba30
serj2626 <serj2626@mail.ru>
2025-02-06 16:33:57 +0300

    feat: add dynamic style

--- src/components/UI/Skeleton.vue
+++ src/components/UI/Skeleton.vue
@@ -1,37 +1,30 @@
 <script lang="ts" setup>
+import { computed } from "vue";
+
 interface IPops {
-  type: "sm" | "md" | "xl";
-  paddingX?: number;
+  type: "sm" | "md" | "xl" | "2xl";
+  marginX?: number;
 }
 
-defineProps<IPops>();
+const props = defineProps<IPops>();
+
+const style = computed(() => {
+  if (props.marginX) {
+    return {
+      marginInline: `${props.marginX - 14}px`,
+    };
+  } else {
+    return {
+      marginInline: "0px",
+    };
+  }
+});
 </script>
 
 <template>
-  <div class="skeleton" :class="{ [`skeleton_${type}`]: type }"></div>
+  <div
+    :style="style"
+    class="skeleton"
+    :class="{ [`skeleton_${type}`]: type }"
+  ></div>
 </template>
-
-<style lang="scss" scoped>
-.skeleton {
-  $paddingX: 14px;
-  margin-inline: 23px - $paddingX;
-  background: linear-gradient(45deg, #3c3c3c 0%, #444444 51%, #333333 100%);
-  border-radius: var(--border-radius-btn);
-
-  &_margin {
-    margin-bottom: 24px;
-  }
-
-  &_sm {
-    height: 10px;
-  }
-
-  &_md {
-    height: 26px;
-  }
-
-  &_xl {
-    height: 36px;
-  }
-}
-</style>
