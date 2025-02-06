6969f05ffd992d1e3ce6e2db6555549c985fe73f
serj2626 <serj2626@mail.ru>
2025-02-06 23:56:02 +0300

    feat: add code

--- src/components/UI/Input.vue
+++ src/components/UI/Input.vue
@@ -1,11 +1,19 @@
-<script lang="ts" setup>
- 
-</script>
+<script lang="ts" setup></script>
 
 <template>
- 
+  <input type="text" class="input" placeholder="Введите количество" />
 </template>
 
 <style lang="scss" scoped>
- 
+.input {
+  &:placeholder {
+    font-size: 14px;
+    font-weight: 500;
+    color: var(--color-white);
+    padding-block: 11px 12px;
+    padding-left: 12px;
+    border: var(--border);
+    border-radius: var(--border-radius-input);
+  }
+}
 </style>
