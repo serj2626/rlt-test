a3d6ea05f9ff614550cc7fa9a9ca7d14bcb476d1
serj2626 <serj2626@mail.ru>
2025-02-06 16:34:32 +0300

    feat: add moke data

--- src/components/CellDetail.vue
+++ src/components/CellDetail.vue
@@ -1,11 +1,78 @@
 <script lang="ts" setup>
-interface IProps {
-  color?: "green" | "blue" | "peru";
-}
+import type { IProduct } from "@/stores/products";
+
+defineProps<{
+  product: IProduct;
+}>();
 </script>
 
 <template>
-  <li class="cell-list__item"></li>
+  <li class="cell">
+    <div :class="{ [`cell_${product.color}`]: product.color }"></div>
+    <div :class="{ [`cell_${product.color}-top`]: product.color }"></div>
+  </li>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.cell {
+  border: var(--border);
+  position: relative;
+  height: 100px;
+  border: var(--border);
+}
+
+.cell_green {
+  position: absolute;
+  top: 29px;
+  left: 26px;
+  width: 48px;
+  height: 48px;
+  background-color: #7faa65;
+}
+
+.cell_green-top {
+  position: absolute;
+  top: 23px;
+  left: 32px;
+  width: 48px;
+  height: 48px;
+  background-color: #B8D998;
+}
+
+
+.cell_peru {
+  position: absolute;
+  top: 29px;
+  left: 26px;
+  width: 48px;
+  height: 48px;
+  background-color: #AA9765;
+}
+
+.cell_peru-top {
+  position: absolute;
+  top: 23px;
+  left: 32px;
+  width: 48px;
+  height: 48px;
+  background-color: #d9bb98b9;
+}
+
+.cell_blue {
+  position: absolute;
+  top: 29px;
+  left: 26px;
+  width: 48px;
+  height: 48px;
+  background-color: #656CAA;
+}
+
+.cell_blue-top {
+  position: absolute;
+  top: 23px;
+  left: 32px;
+  width: 48px;
+  height: 48px;
+  background-color: #7481ED;
+}
+</style>
