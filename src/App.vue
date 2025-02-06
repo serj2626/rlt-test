732ef57cace2391ee72136a4667e478a34a98ab3
serj2626 <serj2626@mail.ru>
2025-02-06 23:16:02 +0300

    style: update grid

--- src/App.vue
+++ src/App.vue
@@ -7,10 +7,11 @@ import Sidebar from "./components/Sidebar.vue";
 <template>
   <div class="app container">
     <Sidebar />
-    <main class="main"><CellList /></main>
-    <footer class="footer">
-      <Footer />
-    </footer>
+    <main class="main">
+      <CellList />
+      <UModal />
+    </main>
+    <Footer />
   </div>
 </template>
 
@@ -18,7 +19,7 @@ import Sidebar from "./components/Sidebar.vue";
 .app {
   display: grid;
   grid-template-columns: 236px 1fr;
-  grid-template-rows: auto 72px;
+  grid-template-rows: 500px 72px;
   gap: 24px;
   grid-template-areas:
     "sidebar main"
@@ -34,5 +35,6 @@ import Sidebar from "./components/Sidebar.vue";
 .main {
   grid-area: main;
   @include block;
+  position: relative;
 }
 </style>
