73d8155cbc1868f47338d223f9638c974a3bbfc4
serj2626 <serj2626@mail.ru>
2025-02-06 16:34:24 +0300

    feat: add moke data

--- src/components/CellList.vue
+++ src/components/CellList.vue
@@ -1,42 +1,53 @@
 <script lang="ts" setup>
 import { useProductStore } from "../stores/products";
 import { storeToRefs } from "pinia";
+import CellDetail from "./CellDetail.vue";
 
 const { products } = storeToRefs(useProductStore());
 </script>
 
 <template>
   <ul class="cell-list">
-    <li class="cell-list__item" v-for="product in products" :key="product.id">
-    
-    </li>
+    <CellDetail
+      v-for="product in products"
+      :key="product.id"
+      :product="product"
+    />
 
+    <div class="cell"></div>
+    <div class="cell"></div>
+    <div class="cell"></div>
+    <div class="cell"></div>
+    <div class="cell"></div>
+    <div class="cell"></div>
+    <div class="cell"></div>
+    <div class="cell"></div>
+    <div class="cell"></div>
+    <div class="cell"></div>
+    <div class="cell"></div>
+    <div class="cell"></div>
+    <div class="cell"></div>
+    <div class="cell"></div>
+    <div class="cell"></div>
+    <div class="cell"></div>
+    <div class="cell"></div>
+    <div class="cell"></div>
+    <div class="cell"></div>
+    <div class="cell"></div>
+    <div class="cell"></div>
+    <div class="cell"></div>
   </ul>
 </template>
 
-<style lang="scss" scoped>
+<style lang="scss" >
 .cell-list {
   display: grid;
   grid-template-columns: repeat(5, 1fr);
-  grid-template-rows: 100px;
   border-radius: var(--border-radius-block);
   overflow: hidden;
   margin: 0;
   padding: 0;
-  height: 100%;
-  width: 100%;
   box-sizing: border-box;
 }
 
-.cell-list__item {
-  border: var(--border);
-  position: relative;
-}
-
-.cell__img{
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-}
 </style>
