4e3de4a77738d897558ce656235d18e32866d6a5
serj2626 <serj2626@mail.ru>
2025-02-07 04:22:54 +0300

    style: formatting code

--- src/components/CellDetail.vue
+++ src/components/CellDetail.vue
@@ -4,19 +4,50 @@ import type { IProduct } from "@/stores/products";
 defineProps<{
   product?: IProduct;
 }>();
+
+const emit = defineEmits<{
+  (e: "dragProduct", item: IProduct): void;
+}>();
+
+function dragOnstart(event: DragEvent, item: IProduct) {
+  if (event.dataTransfer) {
+    event.dataTransfer.dropEffect = "move";
+    event.dataTransfer.effectAllowed = "move";
+    event.dataTransfer.setData("itemId", JSON.stringify(item.id));
+  }
+}
 </script>
 
 <template>
-  <div>
-    <div :class="{ [`cell_${product?.color}`]: product?.color }"></div>
-    <span v-if="product?.count" class="cell__count cell__count_position">{{
-      product?.count
-    }}</span>
-    <div :class="{ [`cell_${product?.color}-top`]: product?.color }"></div>
+  <div
+    v-if="product"
+    class="cell__draggable"
+    draggable="true"
+    @dragstart="dragOnstart($event, product)"
+  >
+    <div class="cell__body">
+      <div :class="{ [`cell_${product?.color}`]: product?.color }"></div>
+      <span v-if="product?.count" class="cell__count cell__count_position">{{
+        product?.count
+      }}</span>
+      <div :class="{ [`cell_${product?.color}-top`]: product?.color }"></div>
+    </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
+.cell__draggable {
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+
+  &:active .cell__body {
+    background-color: greenyellow;
+    border-radius: 20%;
+    border: var(--border);
+  }
+}
+
 .cell__count {
   display: inline-block;
   color: var(--color-white);
