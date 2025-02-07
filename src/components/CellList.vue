0f458fef753907f451dab5d14516462c998536c8
serj2626 <serj2626@mail.ru>
2025-02-07 14:59:16 +0300

    feat: created func by select cell

--- src/components/CellList.vue
+++ src/components/CellList.vue
@@ -1,13 +1,22 @@
 <script setup lang="ts">
-import { ref, onMounted } from "vue";
+import { ref } from "vue";
+import type { IProduct } from "../stores/products";
 import { useProductStore } from "../stores/products";
 import { storeToRefs } from "pinia";
 import CellDetail from "./CellDetail.vue";
+
+const showModal = ref(false);
+const currentProduct = ref<IProduct | null>(null);
 
 const store = useProductStore();
 const { products } = storeToRefs(store);
 const { updateCellId, saveProductsToLocalStorage } = store;
 const cells = ref([...Array(25).keys()].map((x) => x + 1));
+
+function selectProduct(item: IProduct) {
+  currentProduct.value = item;
+  showModal.value = true;
+}
 
 function onDrop(event: DragEvent, cellId: number) {
   if (event.dataTransfer) {
@@ -29,7 +38,11 @@ function onDrop(event: DragEvent, cellId: number) {
       @dragenter.prevent
       @drop="onDrop($event, cell)"
     >
-      <CellDetail :product="products.find((p) => p.cellId === cell)" />
+      <CellDetail
+        :product="products.find((p) => p.cellId === cell)"
+        @select-product="selectProduct"
+      />
     </div>
+    <UModal @close="showModal = false" v-if="showModal" :product="currentProduct" />
   </section>
 </template>
