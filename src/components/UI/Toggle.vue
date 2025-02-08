097ca65e9079d671eecd2e0c81a2999d8ead9382
serj2626 <serj2626@mail.ru>
2025-02-08 16:28:03 +0300

    feat: new component by dark and light mode

---
+++ src/components/UI/Toggle.vue
@@ -0,0 +1,86 @@
+<script lang="ts" setup>
+import { useProductStore } from "@/stores/products";
+import { storeToRefs } from "pinia";
+const store = useProductStore();
+const { mode, modeTitle } = storeToRefs(store);
+const { changeMode } = store;
+</script>
+
+<template>
+  <label class="checkbox" for="checkbox">
+    <input
+      class="checkbox__inp"
+      type="checkbox"
+      id="checkbox"
+      @click="changeMode"
+    />
+    <span class="checkbox__inner">{{ modeTitle }} {{ mode }}</span>
+  </label>
+</template>
+
+<style lang="scss" scoped>
+.checkbox {
+  display: inline-block;
+  position: absolute;
+  top: 50px;
+  left: 50px;
+}
+
+.checkbox__inp {
+  position: absolute;
+  opacity: 0;
+  width: 0;
+  height: 0;
+  appearance: none;
+}
+
+.checkbox__inner {
+  display: inline-flex;
+  position: relative;
+  color: white;
+  font-size: 18px;
+  padding: 8px;
+  padding-left: 74px;
+  text-transform: uppercase;
+}
+
+.checkbox__inner:before {
+  display: block;
+  content: "";
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 60px;
+  height: 36px;
+  border-radius: 18px;
+  background-color: white;
+  transition: background-color 0.3s;
+}
+
+.checkbox__inner::after {
+  display: block;
+  content: "";
+  position: absolute;
+  width: 30px;
+  height: 30px;
+  border-radius: 15px;
+  background-color: rgb(0, 201, 7);
+  left: 3px;
+  top: 3px;
+  transition: background-color 0.3s, left 0.3s;
+}
+
+.checkbox__inp:checked + .checkbox__inner::before {
+  background-color: rgb(0, 201, 7);
+}
+
+.checkbox__inp:checked + .checkbox__inner::after {
+  background-color: white;
+  left: 27px;
+}
+
+.checkbox__inp:focus + .checkbox__inner:before {
+  outline: 1px solid gray;
+  outline-offset: 3px;
+}
+</style>
