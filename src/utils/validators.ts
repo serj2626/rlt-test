430de9e0078846bab34f13d6f4939bb6bc288696
serj2626 <serj2626@mail.ru>
2025-02-08 19:52:33 +0300

    feat: update func

--- src/utils/validators.ts
+++ src/utils/validators.ts
@@ -1,5 +1,5 @@
 function isNumber<T>(value: T): boolean {
-  return typeof value === "number" && !isNaN(value) && value > 0;
+  return typeof value === "number" && !isNaN(value);
 }
 
 export { isNumber };
